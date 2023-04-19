6698784bb13e3c952334b547575380913b351536
Ahmad Alhariri <programer069@gmail.com>
2023-04-19 06:21:41 +0300

    adjust the home page and make it responsive

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as Container } from "./Container/Container";
 export { default as NavItem } from "./NavItem/NavItem";
 export { default as Card } from "./Cards/Card";
 export { default as SectionHeader } from "./SectionHeader/SectionHeader";
+export { default as SubSectionHeader } from "./SectionHeader/SubSectionHeader";
 export { default as SectionWrapper } from "./SectionWrapper/SectionWrapper";
 export { default as PrimaryButton } from "./Buttons/Buttons";
 export { default as CardSidbar } from "./CardSidbar/CardSidbar";
