91f176a88f3584f0df8b69ef881e7888cd1e7e9a
MohammadFarwan <alfrwan58@gmail.com>
2023-04-20 23:26:06 +0300

    container

--- src/sections/Content/TeacherContent.js
+++ src/sections/Content/TeacherContent.js
@@ -3,7 +3,7 @@ const TeacherContent = ({children }) => {
   
   return (
     <>
-      <div className="p-4 mt-24 ">
+      <div className="p-4 mt-24  container mx-auto pl-56 px-32">
         {/* <div className="p-4 border-2 w-full  border-gray-200 border-dashed rounded-lg dark:border-gray-700">  */}
         {children}
         {/* </div> */}
