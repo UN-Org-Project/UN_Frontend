40f0157eeeadc716b3b88ced2544afbe55dec9df
Ahmad Alhariri <programer069@gmail.com>
2023-04-20 17:46:18 +0300

    multple buttons diplay when child details page open

--- src/components/SideBar/MenuBtn/Btn.js
+++ src/components/SideBar/MenuBtn/Btn.js
@@ -6,8 +6,8 @@ const Btn = (props) => {
     <Link
       onClick={props.onClick}
       to={props.path}
-      className={`flex ${
-        props.active ? `bg-teal-500` : ``
+      className={`flex ${props.active ? `bg-teal-500` : ``} ${
+        props.isEnteredn ? ` visible ` : `invisible`
       } items-center text-sm font-medium text-white py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out`}
     >
       <img src={props.icon} className=" w-6 h-6 fill-current " />
